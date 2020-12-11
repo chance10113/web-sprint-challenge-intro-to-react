640fcbbeccfad6e4ff3e200461ca93533dd896c3
Chance Swartz <chanceswartz@gmail.com>
2020-12-11 12:10:26 -0600

    Finally got the app to work with character, just need to figure out a way to map through the array of data

--- src/App.js
+++ src/App.js
@@ -1,50 +1,65 @@
-import React, { useState , useEffect } from 'react';
-import './App.css';
-import axios from 'axios'
-import Character from './components/Character'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const characterDataDefault = {
-id: '', name: '', status: '', species: '', type: '', gender: '', 
-origin: {}, location: {}, image: '', episode: [], url: '', created: '',
-}
+  id: "",
+  name: "",
+  status: "",
+  species: "",
+  type: "",
+  gender: "",
+  origin: {},
+  location: {},
+  image: "",
+  episode: [],
+  url: "",
+  created: "",
+};
 
 const App = () => {
-// BUNCHA CONSTS HERE FOR THE STATE STUFF
-const [ characterData , setCharacterData] = useState(characterDataDefault)
+  // BUNCHA CONSTS HERE FOR THE STATE STUFF
+  const [characterData, setCharacterData] = useState(characterDataDefault);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-// FETCH API HERE!!!   USE AN EFFECT HOOK!!!
-    useEffect(() => {
-      const fetchData = () => {
-        axios.get('https://rickandmortyapi.com/api/character')
+  // FETCH API HERE!!!   USE AN EFFECT HOOK!!!
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character")
 
-        .then(res => {
-          setCharacterData(res.data)
-          console.log('App.js .then' , res.data)
-          console.log('App.js .then.id' , res.data.id)
+        .then((res) => {
+          setCharacterData(res.data.results);
+          console.log("App.js .then", res.data);
+          console.log("App.js .then.id", res.data.results)
+          console.log(characterData);
         })
 
-        .catch(beef => {
-          console.log('Failure in App.js', beef)
-        })
-      }
-      fetchData()
-    }, [] )
+        .catch((beef) => {
+          console.log("Failure in App.js", beef);
+        });
+    };
+    fetchData();
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+    
+
+  }, []);
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-// PUT THE STUFF BELOW, AND DO THE FANCY SHMANCY IN CHARACTER.JS
+  // PUT THE STUFF BELOW, AND DO THE FANCY SHMANCY IN CHARACTER.JS
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {/* < Character characterData = {characterData} /> */}
+      < Character characterData = {characterData} />
     </div>
   );
-}
+};
 
 export default App;
 
@@ -56,4 +71,3 @@ export default App;
 
 // Get multiple characters
 // You can get multiple characters by adding an array of ids as parameter: /character/[1,2,3] or /character/1,2,3
-
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,3 +1,37 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
 
-// export default Character 
+export default function Character({ characterData }) {
+  const {
+    id,
+    name,
+    status,
+    species,
+    type,
+    gender,
+    origin,
+    location,
+    image,
+    episode,
+    url,
+    created,
+  } = characterData;
+
+  return (
+    <header>
+        <h2> STEVE </h2>
+    </header>
+
+
+
+
+  )
+
+  //   GOTTA FIGURE OUT HOW TO DISPLAY ALL Data, AND MAP IT OVER!!!
+  //   MAP HERE OR IN APP?
+  //   PROLLY APP
+}
+
+// MAP IN APP, PASSING THROUGH ID NUMBER,
+//THEN USE ID NUMBER AS IDENTIFIER FOR CHARACTER DISPLAY FUNCTION HERE
