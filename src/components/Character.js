40360801d2b8ee91097e06d80ff32a2853ec1f57
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-22 12:05:48 -0600

    Significantly cleaned up the code.

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,52 +2,38 @@ import React from "react";
 import styled from "styled-components";
 
 export default function characterCard({ characterData }) {
-  const {
-    id,
-    name,
-    status,
-    species,
-    type,
-    gender,
-    //image
-    created,
-  } = characterData;
+  const { id, name, status, species, type, gender, created } = characterData;
 
   return (
     <StyledSection>
       <StyledH3>{name}</StyledH3>
-      <div>
-        <StyledP> Id: {id} </StyledP>
-        <StyledP> Status: {status} </StyledP>
-        <StyledP> Species: {species} </StyledP>
-        {type === "" ? (
-          <StyledP> Type: Normal </StyledP>
-        ) : (
-          <StyledP> Type: {type} </StyledP>
-        )}
-
-        <StyledP> Gender: {gender} </StyledP>
-        {/* <StyledP>   origin: {origin} </StyledP> */}
-        {/* <StyledP>  location: {location} </StyledP> */}
-        {/* <Styledimg alt= 'steve' src = {image} >  </Styledimg> */}
-        {/* <StyledP> episode: {episode} </StyledP>
-        <StyledP> url: {url} </StyledP> */}
-        <StyledP> Created: {created} </StyledP>
-      </div>
+      <StyledDiv>
+        <p> Id: {id} </p>
+        <p> Status: {status} </p>
+        <p> Species: {species} </p>
+        {type === "" ? <p> Type: Normal </p> : <p> Type: {type} </p>}
+        <p> Gender: {gender} </p>
+        <p> Created: {created} </p>
+      </StyledDiv>
     </StyledSection>
   );
 }
 
 const StyledSection = styled.section`
-  display:flex;
-  flex-direction:column;
-  flex-wrap: wrap ;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
   text-align: center;
+  font-size: 1.3rem;
   color: ${(pr) => pr.theme.black};
 `;
 
-const StyledH3 = styled.h3``;
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-const StyledP = styled.p``;
-
-const Styledimg = styled.img``;
+const StyledH3 = styled.h3`
+  font-size: 1.55rem;
+  font-weight: bold;
+`;
