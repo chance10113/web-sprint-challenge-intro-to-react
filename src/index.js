e6c2515e12023a257f1a04e5484c03bbb8c6ea1b
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-22 10:57:14 -0600

    Did work on the theme, got the background image to display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
+import theme from "./Theme-Holder.js";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+    document.getElementById("root")
+  );
